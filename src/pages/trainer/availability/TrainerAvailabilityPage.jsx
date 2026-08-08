9669ff1992caa27756022c84869316baca2b80a4
hashCodersClub <hashcoderslab@gmail.com>
2026-08-08 16:17:59 +0530

    fixed-2

--- src/pages/trainer/availability/TrainerAvailabilityPage.jsx
+++ src/pages/trainer/availability/TrainerAvailabilityPage.jsx
@@ -103,7 +103,7 @@ const TrainerAvailabilityPage = () => {
 
   useEffect(() => {
     loadAvailability();
-  }, []);
+  }, [loadAvailability]);
 
   /*
   |--------------------------------------------------------------------------
